0415b127d20d681175b2f171be1c9faba433707a
André Lohrenz <mail@cataclyst.de>
2017-03-28 23:22:25 +0200

    Some attempts at a tag list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import {Button} from 'semantic-ui-react';
+//import {Button} from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">    
+      <div className="App">
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import '../semantic/dist/semantic.min.css';
+// import '../semantic/dist/semantic.js';
 
 ReactDOM.render(
   <App />,
