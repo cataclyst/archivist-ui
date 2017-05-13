93734fd34ff7ec5c27e6c5a62d493ff23bc06222
André Lohrenz <mail@cataclyst.de>
2017-05-13 21:43:21 +0200

    Changes for dropdown prototype

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import '../public/jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import '../semantic/dist/semantic.min.css';
+import '../semantic/dist/semantic';
 // import '../semantic/dist/semantic.js';
 
 ReactDOM.render(
