c195d56798a1b3de57e4418660383ca19ecb0d04
André Lohrenz <mail@cataclyst.de>
2019-01-04 20:51:13 +0100

    Adapt all files necessary for latest React version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom';
-import Menu from './menu';
+import AppMenu from './menu';
 import RecentChanges from "./recent_changes";
 import TagList from "./tag_list";
 
 const App = () => (
     <BrowserRouter>
         <div>
-            <Menu />
+            <AppMenu />
             <Route path='/' exact component={RecentChanges} />
             <Route path='/tags' component={TagList} />
+            {/* <Route path='/add' component={NewEntry} /> */}
         </div>
     </BrowserRouter>
 );
--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import { Menu, Button } from 'semantic-ui-react';
 
-const Menu = ({match}) => (
+const AppMenu = ({match}) => (
     <div className="ui secondary pointing stackable menu">
         <div className="ui container">
             <div className="header item">Archivist</div>
             <NavLink to="/" exact activeClassName="active" className="item">
                 <i className="bar chart icon"/> Recent documents
             </NavLink>
-            <NavLink to="/documents/add" activeClassName="active" className="item">
-                <i className="add square icon"/> New document
-            </NavLink>
             <NavLink to="/tags" activeClassName="active" className="item">
                 <i className="tags icon"/> Tags
             </NavLink>
+            <Menu.Item>
+                <Button primary>
+                    {/* <i className="add square icon"/>  */}
+                    New document
+                </Button>
+            </Menu.Item>
             <div className="right menu">
                 <NavLink to="/settings" activeClassName="active" className="item">
                     <i className="setting icon"/> Settings
@@ -29,4 +33,4 @@ const Menu = ({match}) => (
     </div>
 );
 
-export default Menu;
+export default AppMenu;
